ae9ab1b1415fa2077259c285f2e595ac265b1174
DaejunBae <dpudpu11@gmail.com>
2019-06-04 16:15:54 +0900

    feat: DB connection, close, 설정 추가

---
+++ booksan/src/main/java/com/booksan/dao/DBUtils.java
@@ -0,0 +1,51 @@
+package com.booksan.dao;
+
+import java.sql.*;
+
+public class DBUtils {
+    private DBUtils() {
+    }
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            PropertiesUtil propertiesUtil = PropertiesUtil.getInstance();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(propertiesUtil.getDbURL(), propertiesUtil.getProperties());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void close(Connection conn, PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        close(conn, ps);
+    }
+}
